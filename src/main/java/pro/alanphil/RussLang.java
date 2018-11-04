e12b7b3404422da5357dae00951ef5816a0d2eda
Alex <filippov.pda@gmail.com>
2018-11-04 22:42:45 +0400

    make groups from file

--- src/main/java/pro/alanphil/RussLang.java
+++ src/main/java/pro/alanphil/RussLang.java
@@ -1,13 +1,55 @@
 package pro.alanphil;
 
+import java.io.*;
+import java.nio.charset.Charset;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import java.util.stream.Stream;
+
 /**
  * Hello world!
  *
  */
-public class RussLang
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+public class RussLang {
+    public static void main(String[] args) throws IOException {
+        Logger logger = Logger.getLogger("Common");
+
+        List<String> file = Files.readAllLines(Paths.get("D:/JavaApps/RusLang/src/main/resources/files/verbs.txt"),
+                Charset.forName("windows-1251"));
+        StringBuilder builder = new StringBuilder(1000);
+        file.forEach(builder::append);
+        String s = new String(builder);
+
+        List<Integer> indexes = new ArrayList<>();
+        for (int index = 0; index < file.size(); index++) {
+            if (file.get(index).isEmpty()) indexes.add(index);
+        }
+        logger.log(Level.INFO, indexes::toString);
+
+        List<List<String>> groups = new ArrayList<>(indexes.size() + 1);
+        List<String> group = new ArrayList<>();
+
+        int currentFileIndex = 0;
+        for (Integer index : indexes) {
+            for (int fileIndex = currentFileIndex; fileIndex <= index; currentFileIndex = ++fileIndex) {
+                if (fileIndex == index) continue;
+                group.add(file.get(fileIndex));
+            }
+            groups.add(group);
+            group = new ArrayList<>();
+        }
+        for (int fileIndex = currentFileIndex; fileIndex < file.size(); fileIndex++) {
+            group.add(file.get(fileIndex));
+        }
+        groups.add(group);
+
+
+        logger.log(Level.INFO, () -> String.valueOf(groups.size()));
+        groups.forEach(list -> logger.log(Level.INFO, list::toString));
     }
 }
