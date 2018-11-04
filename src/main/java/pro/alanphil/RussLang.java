8dd0b7a16d2431db4bbfc2d00e28397e00e37ef0
Alex <filippov.pda@gmail.com>
2018-11-04 10:32:02 +0400

    start project

---
+++ src/main/java/pro/alanphil/RussLang.java
@@ -0,0 +1,13 @@
+package pro.alanphil;
+
+/**
+ * Hello world!
+ *
+ */
+public class RussLang
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ src/test/java/pro/alanphil/RussLangTest.java
@@ -0,0 +1,20 @@
+package pro.alanphil;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple RussLang.
+ */
+public class RussLangTest
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+}
