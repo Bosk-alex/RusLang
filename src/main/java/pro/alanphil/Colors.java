4e275f01d5b10f3f092a613693e129d8868a397f
Alex <filippov.pda@gmail.com>
2018-11-05 20:25:34 +0400

    create Enum Colors: set colors and methods to change console text color

---
+++ src/main/java/pro/alanphil/Colors.java
@@ -0,0 +1,54 @@
+package pro.alanphil;
+
+import static pro.alanphil.RussLang.*;
+
+public enum Colors {
+    BLACK(30), BLUE(34), CYAN(36), GREEN(32), MAGENTA(35), RED(31), WHITE(37), YELLOW(33);
+
+    private final int colorCode;
+
+    Colors(int colorCode) {
+        this.colorCode = colorCode;
+    }
+
+    public int getColorCode() {
+        return colorCode;
+    }
+
+    public static String colorMassage(String text, Colors color) {
+        return colorAllMassages(text, color) + POSTFIX;
+    }
+
+    public static String colorAllMassages(String text, Colors color) {
+        int colorCode;
+        switch (color) {
+            case RED:
+                colorCode = RED.getColorCode();
+                break;
+            case GREEN:
+                colorCode = GREEN.getColorCode();
+                break;
+            case YELLOW:
+                colorCode = YELLOW.getColorCode();
+                break;
+            case BLUE:
+                colorCode = BLUE.getColorCode();
+                break;
+            case MAGENTA:
+                colorCode = MAGENTA.getColorCode();
+                break;
+            case CYAN:
+                colorCode = CYAN.getColorCode();
+                break;
+            case WHITE:
+                colorCode = WHITE.getColorCode();
+                break;
+            case BLACK:
+                colorCode = BLACK.getColorCode();
+                break;
+            default:
+                colorCode = 0;
+        }
+        return PREFIX_ANSI + colorCode + "m" + text;
+    }
+}
