21f1a53061a843ffd79d721efbda0c6f3be5ce40
Alex <filippov.pda@gmail.com>
2018-11-05 20:48:04 +0400

    add result Map of words grouped by word type

--- src/main/java/pro/alanphil/RussLang.java
+++ src/main/java/pro/alanphil/RussLang.java
@@ -5,26 +5,36 @@ import java.io.IOException;
 import java.nio.charset.Charset;
 import java.nio.file.*;
 import java.util.*;
+import java.util.List;
 import java.util.logging.*;
+
+import static pro.alanphil.Colors.*;
 
 /**
  * Hello world!
  *
  */
 public class RussLang {
+    static final String PREFIX_ANSI = "\u001B[";
+    static final String POSTFIX = PREFIX_ANSI + "0m";
     private static final Logger logger = Logger.getLogger("Common");
     private static final Properties properties = new Properties();
 
     public static void main(String[] args) throws IOException {
+        System.err.println(colorAllMassages("Change color of Error massage", CYAN));
         properties.load(new FileInputStream("src/main/resources/RusLang.properties"));
+        Map<String, List<List<String>>> wordGroups = new HashMap<>();
 
         for (Object key : properties.keySet()) {
             if (!key.toString().contains("path")) continue;
             List<String> file = getListFromFile(properties.getProperty(String.valueOf(key)),properties.getProperty("code"));
             List<Integer> indexes = getListOfEmptyIndexes(file);
             List<List<String>> groups = getGroupsFromFile(indexes, file);
-            groups.forEach(list -> logger.log(Level.INFO, list::toString));
+            wordGroups.put(key.toString().substring("pathTo".length()), groups);
+            logger.info(() -> key.toString().substring("pathTo".length()) + " " + groups.size());
         }
+
+        logger.info(wordGroups.toString());
     }
 
     private static List<String> getListFromFile(String fileName, String codeName) throws IOException {
@@ -56,7 +66,6 @@ public class RussLang {
             group.add(file.get(fileIndex));
         }
         groups.add(group);
-        logger.log(Level.INFO, () -> String.valueOf(groups.size()));
         return groups;
     }
 }
