bf84dc5bfd5c40c7e8053c1036c7026111167b6c
Alex <filippov.pda@gmail.com>
2018-11-06 22:44:47 +0400

    verb task about participles

--- src/main/java/pro/alanphil/RussLang.java
+++ src/main/java/pro/alanphil/RussLang.java
@@ -2,70 +2,57 @@ package pro.alanphil;
 
 import java.io.FileInputStream;
 import java.io.IOException;
-import java.nio.charset.Charset;
-import java.nio.file.*;
 import java.util.*;
-import java.util.List;
 import java.util.logging.*;
 
 import static pro.alanphil.Colors.*;
+import static pro.alanphil.FilesToOrFromLists.getGroupsFromFile;
+import static pro.alanphil.FilesToOrFromLists.getListFromFile;
+import static pro.alanphil.FilesToOrFromLists.getListOfEmptyIndexes;
+import static pro.alanphil.VerbsTasks.checkParticipleList;
+import static pro.alanphil.VerbsTasks.groupsWithRepeats;
+import static pro.alanphil.VerbsTasks.removeRepeats;
 
 /**
  * Hello world!
- *
  */
-public class RussLang {
+class RussLang {
+
+    private static final Properties properties = new Properties();
     static final String PREFIX_ANSI = "\u001B[";
     static final String POSTFIX = PREFIX_ANSI + "0m";
-    private static final Logger logger = Logger.getLogger("Common");
-    private static final Properties properties = new Properties();
+    static final Logger logger = Logger.getLogger("Common");
+    static final String STRING_SEPARATOR = "\n";
 
     public static void main(String[] args) throws IOException {
-        System.err.println(colorAllMassages("Change color of Error massage", CYAN));
+        logger.config(() -> colorMessage("New color", BLUE));
+        logger.info(() -> colorAllMassages("Change color of Error massage", BLUE));
         properties.load(new FileInputStream("src/main/resources/RusLang.properties"));
-        Map<String, List<List<String>>> wordGroups = new HashMap<>();
+        Map<String, List<List<String>>> wordGroups = createMapOfLists();
 
+        verbTasks(wordGroups);
+    }
+
+    private static Map<String, List<List<String>>> createMapOfLists() throws IOException {
+        Map<String, List<List<String>>> wordGroups = new HashMap<>();
         for (Object key : properties.keySet()) {
             if (!key.toString().contains("path")) continue;
-            List<String> file = getListFromFile(properties.getProperty(String.valueOf(key)),properties.getProperty("code"));
+            List<String> file = getListFromFile(properties.getProperty(String.valueOf(key)),
+                    properties.getProperty("code"));
             List<Integer> indexes = getListOfEmptyIndexes(file);
             List<List<String>> groups = getGroupsFromFile(indexes, file);
             wordGroups.put(key.toString().substring("pathTo".length()), groups);
-            logger.info(() -> key.toString().substring("pathTo".length()) + " " + groups.size());
         }
-
-        logger.info(wordGroups.toString());
+        return wordGroups;
     }
 
-    private static List<String> getListFromFile(String fileName, String codeName) throws IOException {
-        return Files.readAllLines(Paths.get(fileName), Charset.forName(codeName));
+    private static void verbTasks(Map<String, List<List<String>>> wordGroups) throws IOException {
+        List<List<String>> tempVerbGroups = new ArrayList<>();
+        wordGroups.get("Verbs").forEach(list -> tempVerbGroups.add(removeRepeats(list)));
+        List<List<String>> verbGroups = checkParticipleList(tempVerbGroups);
+
+        FilesToOrFromLists.saveListsToFile(verbGroups, properties.getProperty("outputVerbGroups"));
+        FilesToOrFromLists.saveListToFile(new ArrayList<>(groupsWithRepeats), properties.getProperty("outputVerbRepeat"));
     }
 
-    private static List<Integer> getListOfEmptyIndexes(List<String> file) {
-        List<Integer> indexes = new ArrayList<>();
-        for (int index = 0; index < file.size(); index++) {
-            if (file.get(index).isEmpty()) indexes.add(index);
-        }
-        return indexes;
-    }
-
-    private static List<List<String>> getGroupsFromFile(List<Integer> indexes, List<String> file) {
-        List<List<String>> groups = new ArrayList<>(indexes.size() + 1);
-        List<String> group = new ArrayList<>();
-
-        int currentFileIndex = 0;
-        for (Integer index : indexes) {
-            for (int fileIndex = currentFileIndex; fileIndex <= index; currentFileIndex = ++fileIndex) {
-                if (fileIndex == index) continue;
-                group.add(file.get(fileIndex));
-            }
-            groups.add(group);
-            group = new ArrayList<>();
-        }
-        for (int fileIndex = currentFileIndex; fileIndex < file.size(); fileIndex++) {
-            group.add(file.get(fileIndex));
-        }
-        groups.add(group);
-        return groups;
-    }
 }
